e142744477cd3b72e80c7eb7539230f230d4a143
listcodename <angelzero16@hotmail.com>
2023-09-25 08:55:56 -0300

    Login avance

---
+++ Biblioteca/js/login.js
@@ -0,0 +1,4 @@
+document.getElementById("close-login").addEventListener('click', ()=>{
+    document.getElementById("login").style.position = "absolute";
+    document.getElementById("login").style.right = "-100%";
+});
