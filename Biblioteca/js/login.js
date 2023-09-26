396991e4382b217d6d0961d01f2f57c3997149e2
listcodename <angelzero16@hotmail.com>
2023-09-26 12:15:36 -0300

    Login + js

--- Biblioteca/js/login.js
+++ Biblioteca/js/login.js
@@ -1,4 +1,16 @@
 document.getElementById("close-login").addEventListener('click', ()=>{
-    document.getElementById("login").style.position = "absolute";
-    document.getElementById("login").style.right = "-100%";
+    document.getElementById("login-cont").style.opacity = "0";
+    setTimeout(()=>{
+        document.getElementById("login").style.position = "absolute";
+        document.getElementById("login").style.right = "-100%";
+    
+    },250);
+});
+
+document.getElementById("open-login").addEventListener('click', ()=>{
+    document.getElementById("login").style.position = "fixed";
+    document.getElementById("login").style.right = "0px";
+    setTimeout(()=>{
+        document.getElementById("login-cont").style.opacity = "1";
+    },300);
 });
