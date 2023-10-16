9aad0c7a1d6e2b23a399b9d0b5c3a005308ef25c
listcodename <angelzero16@hotmail.com>
2023-10-16 16:22:39 -0300

    Minor changes + cp update

--- Biblioteca/js/dashboard-menu.js
+++ Biblioteca/js/dashboard-menu.js
@@ -1,13 +1,16 @@
 const showRes = document.querySelector(".menu-item.show-reservas");
 const showUser = document.querySelector(".menu-item.show-usuarios");
+const showLibro = document.querySelector(".menu-item.show-libros");
 
 const frameRes = document.querySelector(".frame-reservas");
 const frameUser = document.querySelector(".frame-usuarios");
+const frameLibro = document.querySelector(".frame-libros");
 
 
 
 showRes.addEventListener("click",()=>{
     frameUser.classList.remove('active');
+    frameLibro.classList.remove('active');
     setTimeout(()=>{
         frameRes.classList.add('active');
     },250);
@@ -15,7 +18,16 @@ showRes.addEventListener("click",()=>{
 
 showUser.addEventListener("click",()=>{
     frameRes.classList.remove('active');
+    frameLibro.classList.remove('active');
     setTimeout(()=>{
         frameUser.classList.add('active');
+    },250);
+});
+
+showLibro.addEventListener("click",()=>{
+    frameUser.classList.remove('active');
+    frameRes.classList.remove('active');
+    setTimeout(()=>{
+        frameLibro.classList.add('active');
     },250);
 });
